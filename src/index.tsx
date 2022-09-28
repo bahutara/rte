b7635ba322dad7733dd6b7c31ad7fe01ae0e6f49
Aksar La'ode <101698503+laodeaksarr@users.noreply.github.com>
2022-09-29 00:39:38 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import Editor, { Quill } from 'react-quill';
+import CustomEditor from './components/Editor';
 
 import { RichTextEditor } from './components/RichTextEditor/RichTextEditor';
 
-export { RichTextEditor, Quill, Editor };
+export { RichTextEditor, Quill, Editor, CustomEditor };
 export default RichTextEditor;
 
 export { Toolbar } from './components/Toolbar/Toolbar';
 
-export { default as Editorjs } from './components/Editor';
